30c8dd24229e2daf6d2b5809ca49a939d16c8bc1
Yos <yoshida.arw.ak@gmail.com>
2021-02-22 23:21:10 +0900

    ランキングの実装完了

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
 'use strict';
+// FS（File System）モジュールを読み込んで使えるようにする
+const fs = require('fs');
+// readlineモジュールを読み込んで使えるようにする
+const readline = require('readline');
+// popu-pref.csvをファイルとして読み込める状態に準備する
+const rs = fs.createReadStream('./popu-pref.csv');
+// readlineモジュールにrsを設定する
+const rl = readline.createInterface({ input: rs, output: {} });
+// popu-pref.csvのデータを1行ずつ読み込んで、設定された関数を実行する
+const prefectureDataMap = new Map();
+rl.on('line', lineString => {
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const prefecture = columns[1];
+    const popu = parseInt(columns[3]);
+    if (year === 2010 || year === 2015){
+        // 都道府県ごとのデータをつくる
+        let value = prefectureDataMap.get(prefecture);
+        if (!value){
+            value = {
+                popu10: 0,
+                popu15: 0,
+                change: null
+            };
+        }
+        if (year === 2010){
+            value.popu10 = popu;
+        }
+        if (year === 2015){
+            value.popu15 = popu;
+        }
+        prefectureDataMap.set(prefecture, value);
+    }
+});
+rl.on('close', () => {
+    for (let [key, value] of prefectureDataMap){
+        value.change = value.popu15 / value.popu10;
+    }
+    const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+        return pair2[1].change - pair1[1].change;
+    });
+    const rankingStrings = rankingArray.map(([key, value]) => {
+        return (
+            key +
+            ': ' +
+            value.popu10 +
+            '=>' +
+            value.popu15 +
+            '変化率:' +
+            value.change
+        );
+    });
+    console.log(rankingStrings);
+});
